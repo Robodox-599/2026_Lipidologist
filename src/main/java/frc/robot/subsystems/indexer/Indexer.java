fbec4dbc378090cf192ef95f3afe01756e7b8dae
Andre S <altsalehi1122@gmail.com>
2026-01-13 15:00:29 -0800

    added all empty indexer files

--- src/main/java/frc/robot/subsystems/indexer/Indexer.java
+++ src/main/java/frc/robot/subsystems/indexer/Indexer.java
@@ -1,5 +1,87 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems.indexer;
 
-public class Indexer {
+import dev.doglog.DogLog;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Indexer extends SubsystemBase {
+  private final IndexerIO io;
+  private WantedState wantedState = WantedState.STOPPED;
+  private CurrentState currentState = CurrentState.STOPPED;
+
+  public enum WantedState{
+    RECIEVE,
+    TRANSFER_FUEL,
+    PULSE_FUEL,
+    STOPPED,
+  }
+
+  public enum CurrentState{
+    RECIEVE,
+    TRANSFER_FUEL,
+    PULSE_FUEL,
+    STOPPED,
+  }
+
+  /** Creates a new indexer. */
+  public Indexer(IndexerIO io) {
+    this.io = io;
+  }
+
+  public void updateInputs() {
+    io.updateInputs();
+    
+    handleStateTransitions();
+    applyStates();
+
+    DogLog.log("Indexer/wantedState", wantedState);
+    DogLog.log("Indexer/currentState", currentState);
+
+  }
+
+  public void handleStateTransitions(){
+    switch (currentState) {
+        case STOPPED:
+            currentState = CurrentState.STOPPED;
+            break;
+        case RECIEVE:
+            currentState = CurrentState.RECIEVE;
+        case TRANSFER_FUEL:
+            currentState = CurrentState.TRANSFER_FUEL;
+        case PULSE_FUEL:
+            currentState = CurrentState.PULSE_FUEL;
+        default:
+            currentState = CurrentState.STOPPED;
+            break;
+    }
+  }
+
+  public void applyStates(){
+    switch(currentState){
+      
+      default:
+        stop();
+        break;
+    }
+  }
+
+  public void setVelocity(double velocity){
+    io.setVelocity(velocity);
+  }
+
+  public void setVoltage(double volts){
+    io.setVoltage(volts);
+  }
+
+  public void stop(){
+    io.stop();
+  }
+
+  public void setWantedState(WantedState wantedState){
+    this.wantedState = wantedState;
+  }
 
 }
---
+++ src/main/java/frc/robot/subsystems/indexer/IndexerIO.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems.indexer;
+
+public abstract class IndexerIO {
+    protected double position = 0.0;
+    protected double velocity = 0.0;
+    protected double statorCurrent = 0.0;
+    protected double supplyCurrent = 0.0;
+    protected double appliedVolts = 0.0;
+    protected double tempCelsius = 0.0;
+
+    public void updateInputs() {}
+
+    public void setVelocity(double velocity) {}
+
+    public void setVoltage(double volts) {}
+
+    public void stop() {}
+}
