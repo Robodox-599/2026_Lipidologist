bf02069e1c215a5ba5f305e995132b9d0e49edaa
Andre S <altsalehi1122@gmail.com>
2026-01-27 15:33:33 -0800

    added rough climb sim layer

--- src/main/java/frc/robot/subsystems/climb/ClimbIOSim.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbIOSim.java
@@ -1,17 +1,73 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems.climb;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import dev.doglog.DogLog;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.simulation.ElevatorSim;
 
-public class ClimbIOSim extends SubsystemBase {
-  /** Creates a new ClimbIOSim. */
-  public ClimbIOSim() {}
+public class ClimbIOSim extends ClimbIO {
+  private final PIDController positionController;
+  private double targetPositionInches = 0.0;
+
+  private static final DCMotor climbGearbox = DCMotor.getKrakenX60Foc(2);
+  private static final ElevatorSim climbSim =
+      new ElevatorSim(
+          climbGearbox,
+          6,
+          Units.lbsToKilograms(28),
+          Units.inchesToMeters(1),
+          Units.inchesToMeters(0),
+          Units.inchesToMeters(89),
+          true,
+          0);
+
+  public ClimbIOSim() {
+    positionController =
+        new PIDController(
+            ClimbConstants.simKP, ClimbConstants.simKI, ClimbConstants.simKD);
+    positionController.setTolerance(ClimbConstants.positionToleranceInches);
+  }
 
   @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  public void updateInputs() {
+    climbSim.update(0.02);
+
+    super.velocityInchesPerSec = (Units.metersToInches(climbSim.getVelocityMetersPerSecond()));
+    super.targetPositionInches = targetPositionInches;
+    super.positionInches = (Units.metersToInches(climbSim.getPositionMeters()));
+    super.appliedVolts = climbSim.getCurrentDrawAmps();
+    super.tempCelsius = 25.0;
+
+    DogLog.log("Climb/VelocityInchesPerSec", super.velocityInchesPerSec);
+    DogLog.log("Climb/TargetPositionInches", super.targetPositionInches);
+    DogLog.log("Climb/PositionInches", super.positionInches);
+    DogLog.log("Climb/AppliedVoltages", super.appliedVolts);
+    DogLog.log("Climb/TempCelcius", super.tempCelsius);
+  }
+
+   @Override
+  public void setClimbHeight(double height) {
+    targetPositionInches = 
+      MathUtil.clamp
+        (ClimbConstants.convertToTicks(height), ClimbConstants.climbLowerLimit, ClimbConstants.climbUpperLimit);
+
+    positionInches = targetPositionInches;
+  }
+
+  @Override
+  public void stopClimb() {
+    climbSim.setInputVoltage(0);
+  }
+
+  @Override
+  public void setClimbVoltage(double voltage) {
+    climbSim.setInputVoltage(voltage);
+  }
+
+  @Override
+  public void zeroClimbPosition() {
+    targetPositionInches = 0.0;
   }
 }
