42131106dbf19c32694acc1ae015e54d83ce9e7c
Andre S <altsalehi1122@gmail.com>
2026-01-27 15:17:13 -0800

    updated io layer

--- src/main/java/frc/robot/subsystems/climb/ClimbIO.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbIO.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems.climb;
 
 public abstract class ClimbIO {
 
-  // left motor
   protected double velocityInchesPerSec = 0.0;
   protected double appliedVolts = 0.0;
   protected double tempCelsius = 0.0;
@@ -11,26 +10,13 @@ public abstract class ClimbIO {
   protected double statorCurrent = 0.0;
   protected double supplyCurrent = 0.0;
 
-  // right motor
-  protected double rightMotorVelocityInchesPerSec = 0.0;
-  protected double rightMotorAppliedVolts = 0.0;
-  protected double rightMotorTempCelsius = 0.0;
-  protected double rightMotorPositionInches = 0.0;
-  protected double rightMotorTargetPositionInches = 0.0;
-  protected double rightMotorStatorCurrent = 0.0;
-  protected double rightMotorSupplyCurrent = 0.0;
-
   public void updateInputs() {}
 
   public void setClimbHeight(double height) {}
-
-  public void setRightClimbHeight(double height) {}
 
   public void stopClimb() {}
 
   public void zeroClimbPosition() {}
 
   public void setClimbVoltage(double voltage) {}
-
-  public void setRightClimbVoltage(double voltage) {}
 }
