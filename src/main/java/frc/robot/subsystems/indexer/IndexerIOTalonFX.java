4043211fe845d52158cb0ea74c2bd8d81f027600
Joshua <happyjoshua0123@gmail.com>
2026-02-23 17:20:37 -0800

    unfinished3

--- src/main/java/frc/robot/subsystems/indexer/IndexerIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerIOTalonFX.java
@@ -21,5 +21,71 @@ import edu.wpi.first.wpilibj.Timer;
 import frc.robot.util.PhoenixUtil;
 
 public class IndexerIOTalonFX extends IndexerIO {
-  
+  private CANBus canbus;
+  private TalonFX indexerMotor;
+  private TalonFXConfiguration indexerConfig;
+
+  private StatusSignal<AngularVelocity> indexerVelocity;
+  private StatusSignal<Current> indexerStatorCurrent;
+  private StatusSignal<Current> indexerSupplyCurrent;
+  private StatusSignal<Voltage> indexerVoltage;
+  private StatusSignal<Temperature> indexerTemperature;
+
+  public IndexerIOTalonFX(){
+    canbus = new CANBus(IndexerConstants.indexerCanbus);
+    indexerMotor = new TalonFX(IndexerConstants.indexerMotorID, canbus);
+    indexerConfig = new TalonFXConfiguration()
+      .withCurrentLimits(
+        new CurrentLimitsConfigs()
+          .withStatorCurrentLimit(IndexerConstants.statorCurrent)
+          .withSupplyCurrentLimit(IndexerConstants.supplyCurrent)
+      ).withMotorOutput(
+        new MotorOutputConfigs()
+          .withInverted(InvertedValue.Clockwise_Positive)
+      );
+    
+    PhoenixUtil.tryUntilOk(10, () -> indexerMotor.getConfigurator().apply(indexerConfig));
+
+    indexerVelocity = indexerMotor.getVelocity();
+    indexerStatorCurrent = indexerMotor.getStatorCurrent();
+    indexerSupplyCurrent= indexerMotor.getSupplyCurrent();
+    indexerVoltage = indexerMotor.getMotorVoltage();
+    indexerTemperature = indexerMotor.getDeviceTemp();
+
+    BaseStatusSignal.setUpdateFrequencyForAll(50, indexerVelocity, indexerStatorCurrent, indexerSupplyCurrent, indexerVoltage, indexerTemperature);
+    indexerMotor.optimizeBusUtilization();
+  }
+
+  @Override
+  public void updateInputs(){
+    BaseStatusSignal.refreshAll(indexerVelocity, indexerStatorCurrent, indexerSupplyCurrent, indexerVoltage, indexerTemperature);
+
+    super.velocity = indexerVelocity.getValueAsDouble();
+    super.statorCurrent = indexerStatorCurrent.getValueAsDouble();
+    super.supplyCurrent = indexerSupplyCurrent.getValueAsDouble();
+    super.voltage = indexerVoltage.getValueAsDouble();
+    super.temperature = indexerTemperature.getValueAsDouble();
+
+    DogLog.log("Indexer/Velocity", super.velocity);
+    DogLog.log("Indexer/StatorCurrent", super.statorCurrent);
+    DogLog.log("Indexer/SupplyCurrent", super.supplyCurrent);
+    DogLog.log("Indexer/Voltage", super.voltage);
+    DogLog.log("Indexer/Temperature", super.temperature);
+  }
+
+  @Override
+  public void stop(){
+    indexerMotor.setVoltage(0);
+  }
+
+  @Override
+  public void setVoltage(double voltage){
+    indexerMotor.setVoltage(voltage);
+  }
+
+  @Override
+  public void pulse(boolean pulse){
+    //activates and deactives motor again and again
+    
+  }
 }
